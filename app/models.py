db18371675d6f29c962c03b121a335466e7b83e9
Dickson Karis <dicksonkariuki4@gmail.com>
2019-10-13 21:30:56 +0300

    Add Articles class instances

--- app/models.py
+++ app/models.py
@@ -9,4 +9,17 @@ class Source:
         self.url = url 
         self.category = category 
         self.country = country
-        
+
+class Articles:
+    """
+    Articles class to define the News article objects
+    """
+    def __init__(self,id, name,title, author,description,url,urlToImage,publishedAt)
+        self.id = id 
+        self.name = name
+        self.title = title
+        self.author = author
+        self.description = description
+        self.url = url
+        self.urlToImage = urlToImage
+        self.publishedAt = publishedAt
