992561dcd4eb17775e18b6a91262c970008848f1
Dickson Karis <dicksonkariuki4@gmail.com>
2019-10-14 08:17:53 +0300

    Create request data

--- app/main/views.py
+++ app/main/views.py
@@ -1,3 +1,17 @@
 from flask import render_template
 from . import main 
 from ..requests import get_sources,get_articles
+# views
+@main.route('/')
+def index ():
+    """
+    Function that returns the index page and its content
+    """
+    business_list = get_sources('gb','business')
+    entertainment_list = get_sources('gb','entertainment')
+    health_list = get_sources('gb','health')
+    science_list = get_sources('gb','science')
+    sports_list = get_sources('gb','sports')
+    technology_list = get_sources('gb','technology')
+    test_args ='working'
+    return render_template('index.html',test_param = test_args,entertainment = entertainment_list,health = health_list,science = science_list, sports = sports_list,technology = technology_list)
