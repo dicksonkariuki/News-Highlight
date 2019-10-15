caa1293912b2a2d9757e8f535aea26cad350bb9a
Dickson Karis <dicksonkariuki4@gmail.com>
2019-10-15 14:40:02 +0300

    Deployment to heroku

--- app/request.py
+++ app/request.py
@@ -26,7 +26,7 @@ def get_sources(coutry, category):
     """
     Function that gets the json response to our url request
     """
-    get_news_url = base_url.format(coutry, category, api_key)
+    get_news_url = base_url.format(country, category, api_key)
     with urllib.request.urlopen(get_news_url) as url:
         get_news_data = url.read()
         get_news_response = json.loads(get_news_data)
