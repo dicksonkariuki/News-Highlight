34f432fe024261e697f60fe1724db4f97a6952a3
Dickson Karis <dicksonkariuki4@gmail.com>
2019-10-15 09:48:57 +0300

    Add  readme headlines

---
+++ README.md
@@ -0,0 +1,59 @@
+News Source App
+=======================
+
+
+**A simple app that lets you choose your preferred news source. View all the top headlines from the source.**
+
+
+The site welcomes you with a simple styled u.i, with a jumbotron. The buttons display the category of news sources to view from. Just;
+
+
+- Tap a category to collapse a list of sources. You can tap it again to close the categories sources.
+- Choose your preferred news source.
+- This will take you to that source's top headlines.
+- Browse through the articles and get an description of each article.
+- Finally click on the link to read the articles form source.
+
+####Technologies used
+The technoligies used in building this site were  
+1. `HTML`
+2. `CSS`
+3. `Python`
+4. `Flask`
+5. `Bootstrap 4`
+6. `News Api`
+
+
+Click [here]() to see the site.
+
+------------
+
+## Author
+
+Dickson Kariuki
+
+
+License
+-------
+
+MIT License
+
+Copyright (c) [2019] [Dickson Kariuki]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- get-pip.py
+++ get-pip.py
@@ -1,25 +1,3 @@
-#!/usr/bin/env python
-#
-# Hi There!
-# You may be wondering what this giant blob of binary data here is, you might
-# even be worried that we're up to something nefarious (good for you for being
-# paranoid!). This is a base85 encoding of a zip file, this zip file contains
-# an entire copy of pip.
-#
-# Pip is a thing that installs packages, pip itself is a package that someone
-# might want to install, especially if they're looking to run this get-pip.py
-# script. Pip has a lot of code to deal with the security of installing
-# packages, various edge cases on various platforms, and other such sort of
-# "tribal knowledge" that has been encoded in its code base. Because of this
-# we basically include an entire copy of pip inside this blob. We do this
-# because the alternatives are attempt to implement a "minipip" that probably
-# doesn't do things correctly and has weird edge cases, or compress pip itself
-# down into a single file.
-#
-# If you're wondering how this is created, it is using an invoke task located
-# in tasks/generate.py called "installer". It can be invoked by using
-# ``invoke generate.installer``.
-
 import os.path
 import pkgutil
 import shutil
@@ -27,7 +5,6 @@ import sys
 import struct
 import tempfile
 
-# Useful for very coarse version differentiation.
 PY2 = sys.version_info[0] == 2
 PY3 = sys.version_info[0] == 3
 
@@ -83,13 +60,10 @@ def bootstrap(tmpdir=None):
     from pip.commands.install import InstallCommand
     from pip.req import InstallRequirement
 
-    # Wrapper to provide default certificate with the lowest priority
+
     class CertInstallCommand(InstallCommand):
         def parse_args(self, args):
-            # If cert isn't specified in config or environment, we provide our
-            # own certificate through defaults.
-            # This allows user to specify custom cert anywhere one likes:
-            # config, environment variable or argv.
+            
             if not self.parser.get_default_values().cert:
                 self.parser.defaults["cert"] = cert_path  # calculated below
             return super(CertInstallCommand, self).parse_args(args)
@@ -145,7 +119,6 @@ def bootstrap(tmpdir=None):
         elif implicit_wheel and req.name == "wheel":
             implicit_wheel = False
 
-    # Add any implicit installations to the end of our args
     if implicit_pip:
         args += ["pip"]
     if implicit_setuptools:
@@ -156,7 +129,7 @@ def bootstrap(tmpdir=None):
     delete_tmpdir = False
     try:
         # Create a temporary directory to act as a working directory if we were
-        # not given one.
+        
         if tmpdir is None:
             tmpdir = tempfile.mkdtemp()
             delete_tmpdir = True
